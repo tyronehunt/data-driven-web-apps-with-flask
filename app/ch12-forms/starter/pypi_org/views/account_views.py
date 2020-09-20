3e4ac1c8a38dc26935243a3f2c3730f5084de59a
Tyrone Hunt <tyrone.hunt07@gmail.com>
2020-09-20 15:23:45 +0100

    ch12 on html forms and user input complete!

--- app/ch12-forms/starter/pypi_org/views/account_views.py
+++ app/ch12-forms/starter/pypi_org/views/account_views.py
@@ -1,6 +1,9 @@
 import flask
 
+from pypi_org.infrastructure import request_dict
 from pypi_org.infrastructure.view_modifiers import response
+from pypi_org.services import user_service
+import pypi_org.infrastructure.cookie_auth as cookie_auth
 
 blueprint = flask.Blueprint('account', __name__, template_folder='templates')
 
@@ -11,7 +14,18 @@ blueprint = flask.Blueprint('account', __name__, template_folder='templates')
 @blueprint.route('/account')
 @response(template_file='account/index.html')
 def index():
-    return {}
+    user_id = cookie_auth.get_user_id_via_auth_cookie(flask.request)
+    if user_id is None:
+        return flask.redirect('/account/login')
+
+    user = user_service.find_user_by_id(user_id)
+    if not user:
+        return flask.redirect('/account/login')
+
+    return {
+        'user': user,
+        'user_id': user.id,
+    }
 
 
 # ################### REGISTER #################################
@@ -19,13 +33,43 @@ def index():
 @blueprint.route('/account/register', methods=['GET'])
 @response(template_file='account/register.html')
 def register_get():
-    return {}
+    return {
+        'user_id': cookie_auth.get_user_id_via_auth_cookie(flask.request),
+    }
 
 
 @blueprint.route('/account/register', methods=['POST'])
 @response(template_file='account/register.html')
 def register_post():
-    return {}
+    data = request_dict.create(default_val='')
+
+    name = data.name
+    email = data.email.lower().strip()
+    password = data.password.strip()
+
+    if not name or not email or not password:
+        return {
+            'name': name,
+            'email': email,
+            'password': password,
+            'error': "Some required fields are missing.",
+            'user_id': cookie_auth.get_user_id_via_auth_cookie(flask.request),
+        }
+
+    user = user_service.create_user(name, email, password)
+    if not user:
+        return {
+            'name': name,
+            'email': email,
+            'password': password,
+            'error': "A user with that email already exists.",
+            'user_id': cookie_auth.get_user_id_via_auth_cookie(flask.request),
+        }
+
+    resp = flask.redirect('/account')
+    cookie_auth.set_auth(resp, user.id)
+
+    return resp
 
 
 # ################### LOGIN #################################
@@ -39,11 +83,39 @@ def login_get():
 @blueprint.route('/account/login', methods=['POST'])
 @response(template_file='account/login.html')
 def login_post():
-    return {}
+    data = request_dict.create(default_val='')
+
+    email = data.email.lower().strip()
+    password = data.password.strip()
+
+    if not email or not password:
+        return {
+            'email': email,
+            'password': password,
+            'error': "Some required fields are missing.",
+            'user_id': cookie_auth.get_user_id_via_auth_cookie(flask.request),
+        }
+
+    user = user_service.login_user(email, password)
+    if not user:
+        return {
+            'email': email,
+            'password': password,
+            'error': "The account does not exist or the password is wrong.",
+            'user_id': cookie_auth.get_user_id_via_auth_cookie(flask.request),
+        }
+
+    resp = flask.redirect('/account')
+    cookie_auth.set_auth(resp, user.id)
+
+    return resp
 
 
 # ################### LOGOUT #################################
 
 @blueprint.route('/account/logout')
 def logout():
-    return {}
+    resp = flask.redirect('/')
+    cookie_auth.logout(resp)
+
+    return resp
